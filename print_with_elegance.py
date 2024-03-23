bb078d7cf78edcfbd91f7aa54eb432b18f7d1320
Thea <uythea487102@gmail.com>
2024-03-23 19:52:57 +0800

    Defined the font style of the variables

--- print_with_elegance.py
+++ print_with_elegance.py
@@ -2,6 +2,8 @@
 #date__march_23_2024
 #this program will ask the user for their name, dream job, hobby, favorite quote, and their personality type
 
+#import modules
+import pyfiglet
 
 #ask the user for their name, dream job, hobby, favorite quote, and their personality type
 name = input("Enter your name: ")
@@ -11,9 +13,22 @@ favorite_quote = input("Enter your favorite quote: ")
 personality_type = input("Enter your personality type: ")
 
 #add a message for the user 
-message = ("you will reach your dream, Believe it!")
+message = ("You will reach your dream, Believe it!")
 
 #define the font style of the variables
+name = (pyfiglet.figlet_format(name,font='invita'))
+dream_job = (pyfiglet.figlet_format(dream_job,font='invita'))
+hobby = (pyfiglet.figlet_format(hobby,font='contessa'))
+personality_type = (pyfiglet.figlet_format(personality_type,font='contessa'))
+favorite_quote = (pyfiglet.figlet_format(favorite_quote,font='digital'))
+message = (pyfiglet.figlet_format(message,font='digital'))
+
+print(name)
+print(dream_job)
+print(hobby)
+print(personality_type)
+print(favorite_quote)
+print(message)
 
 #define the font color of the variables
 
@@ -21,3 +36,4 @@ message = ("you will reach your dream, Believe it!")
 
 #this is the end of the program
 #thank you for using my program
+
