893a11d93bb349bf0f1e55b28a7e4e9f480943bf
Thea <uythea487102@gmail.com>
2024-03-23 20:21:33 +0800

    define the font color of the variables

--- print_with_elegance.py
+++ print_with_elegance.py
@@ -4,6 +4,8 @@
 
 #import modules
 import pyfiglet
+import colorama
+colorama.init(autoreset=True)
 
 #ask the user for their name, dream job, hobby, favorite quote, and their personality type
 name = input("Enter your name: ")
@@ -23,14 +25,21 @@ personality_type = (pyfiglet.figlet_format(personality_type,font='contessa'))
 favorite_quote = (pyfiglet.figlet_format(favorite_quote,font='digital'))
 message = (pyfiglet.figlet_format(message,font='digital'))
 
+#define the font color of the variables
+name = (colorama.Fore.LIGHTMAGENTA_EX + name)
+dream_job = (colorama.Fore.LIGHTCYAN_EX + dream_job)
+hobby = (colorama.Fore.LIGHTYELLOW_EX + hobby)
+personality_type = (colorama.Fore.LIGHTCYAN_EX + personality_type)
+favorite_quote = (colorama.Fore.LIGHTMAGENTA_EX + favorite_quote)
+message = (colorama.Fore.LIGHTBLUE_EX + message)
+
 print(name)
 print(dream_job)
 print(hobby)
 print(personality_type)
 print(favorite_quote)
-print(message)
-
-#define the font color of the variables
+print(message
+      )
 
 #display the data
 
