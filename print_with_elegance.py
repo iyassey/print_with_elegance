ccb27cf41078cbbede7bcac7a5994a643309890b
Thea <uythea487102@gmail.com>
2024-03-23 18:01:01 +0800

    Created the flow of the program using pseudocode

---
+++ print_with_elegance.py
@@ -0,0 +1,16 @@
+#author__thea_uy
+#date__march_23_2024
+#this program will ask the user for their name, dream job, hobby, favorite quote, and their personality type
+
+#ask the user for their name, dream job, hobby, favorite quote, and their personality type
+
+#add a message for the user 
+
+#define the font style of the variables
+
+#define the font color of the variables
+
+#display the data
+
+#this is the end of the program
+#thank you for using my program
