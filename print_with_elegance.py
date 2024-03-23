9b0d2a21502f25688ef84a08e956ad80e86babb0
Thea <uythea487102@gmail.com>
2024-03-23 18:14:36 +0800

    Asked the user for their name, dream job, hobby, favorite quote, and personality type

--- print_with_elegance.py
+++ print_with_elegance.py
@@ -2,7 +2,13 @@
 #date__march_23_2024
 #this program will ask the user for their name, dream job, hobby, favorite quote, and their personality type
 
+
 #ask the user for their name, dream job, hobby, favorite quote, and their personality type
+name = input("Enter your name: ")
+dream_job = input("Enter your dream job: ")
+hobby = input("Enter your hobby: ")
+favorite_quote = input("Enter your favorite quote: ")
+personality_type = input("Enter your personality type: ")
 
 #add a message for the user 
 
